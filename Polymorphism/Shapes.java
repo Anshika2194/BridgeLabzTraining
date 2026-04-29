109905782ba69e29ddcb437078b5613f1f258b68
Anshika2194 <srianshika2194@gmail.com>
2026-04-29 21:20:12 +0530

    Added polymorphism

---
+++ Polymorphism/Shapes.java
@@ -0,0 +1,6 @@
+package Polymorphism;
+public class Shapes {
+    void area(){
+        System.out.println("I am in shapes ");
+    }
+}
---
+++ Polymorphism/Triangle.java
@@ -0,0 +1,9 @@
+package Polymorphism;
+
+public class Triangle extends Shapes{
+    void area(){
+        System.out.println("Area is 0.5 * base * height");
+    }
+}
+  
+
