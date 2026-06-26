cf0bc2a3ef330c25827c788e65ae10ad44c62839
Anshika2194 <srianshika2194@gmail.com>
2026-06-26 13:45:01 +0530

    added review 7 sql queries

--- Solution2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class Solution2 {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        int ans=nums[nums.length-k];
-        return ans;
-        
-    }
-}
